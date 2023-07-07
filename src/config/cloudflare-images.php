d3ed6fe71e319ee7b2b143603268cb371d4971e0
Alex Buckham <alexbuckham@me.com>
2023-07-08 09:59:38 +1000

    fix: add variant config

--- src/config/cloudflare-images.php
+++ src/config/cloudflare-images.php
@@ -5,4 +5,13 @@ return [
 	'token'        => env('CF_IMAGES_TOKEN', null),
 	'key'          => env('CF_IMAGES_KEY', null),
 	'delivery_url' => env('CF_IMAGES_DELIVERY_URL', null),
+	'variants' => [
+		// 'small'           => [
+		// 	'width'         => 400,
+		// 	'height'        => 400,
+		// 	'always_public' => false,
+		// 	'blur'          => null,
+		// 	'metadata'          => false,
+		// ],
+	]
 ];
